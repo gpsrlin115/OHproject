9c4d03aa48fb53d5589c7b7d83c2140e50f2362c
gpsrlin115 <lefd123@gmail.com>
2024-01-01 00:42:19 +0900

    fix : DB connection

--- src/main/java/com/example/ourhome/webProject/model/Product.java
+++ src/main/java/com/example/ourhome/webProject/model/Product.java
@@ -17,7 +17,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private String descritpion;
+    private String description;
     private Long price;
     private int discount;
 
