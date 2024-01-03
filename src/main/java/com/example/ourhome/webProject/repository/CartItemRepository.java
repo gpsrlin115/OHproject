f7d9671d5b1ca90cb3c3a0e3220e943c46a5b133
gpsrlin115 <lefd123@gmail.com>
2024-01-03 17:56:37 +0900

    fix : 리팩토링

--- src/main/java/com/example/ourhome/webProject/repository/CartItemRepository.java
+++ src/main/java/com/example/ourhome/webProject/repository/CartItemRepository.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByProduct(Product product);
 }
