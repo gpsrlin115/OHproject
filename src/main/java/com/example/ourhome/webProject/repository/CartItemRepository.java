39a7375743603f8d5ca12733ac14c644185a56bc
gpsrlin115 <lefd123@gmail.com>
2024-01-03 09:46:18 +0900

    fix : 불필요한 js 제거, 클라이언트 단에서 계산되게 변경

--- src/main/java/com/example/ourhome/webProject/repository/CartItemRepository.java
+++ src/main/java/com/example/ourhome/webProject/repository/CartItemRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByProduct(Product product);
 }
--- src/main/java/com/example/ourhome/webProject/repository/ProductRepository.java
+++ src/main/java/com/example/ourhome/webProject/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.example.ourhome.webProject.repository;
 
 import com.example.ourhome.webProject.model.Product;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
